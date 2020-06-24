debb8062f07c4c11256dc9b0f64223f9acfedd4b
flisko <ziga.flis@student.um.si>
2020-06-24 14:01:47 +0200

    fixed graphs

--- src/app/components/Wallet/SelectedWallet/index.js
+++ src/app/components/Wallet/SelectedWallet/index.js
@@ -238,7 +238,7 @@ class SelectedWallet extends Component {
                                                 }
 
                                             </div>
-                                            <div className="chartctn" style={{marginBottom:"30px"}}>
+                                            <div className="chartctn">
                                                 <ResponsiveContainer width={'100%'} height={200}>
                                                     <AreaChart data={sparkline} baseValue={dataMin}>
                                                         <defs>
